c4f8e83b36202dc83008612f389e9bac16258c5b
Preda Radu-Alexandru <36919258+DonCo007@users.noreply.github.com>
2019-04-11 11:07:44 +0300

    Add files via upload

---
+++ Tema_3/Source.cpp
@@ -0,0 +1,104 @@
+#include"Facultate.h"
+
+
+void main()
+{
+	short nrGp;
+
+	cout << "Numarul de grupe: ";
+	cin >> nrGp;
+	CFacultate ACE(nrGp);
+
+	short op = 9;
+
+	while (op)
+	{
+		cout << "\n 0 -> Exit"
+			<< "\n 1-> Afisare studenti grupa"
+			<<"\n 2-> Adaugare"
+			<<"\n 3-> Afisare studenti din afara Craiovei"
+			<<"\n 4-> Orar student"
+			<<"\n 5-> Afisare grupe cu materii comune"
+			<<"\n 6-> Afisare studenti dupa data"
+			<<'\n';
+
+		cin >> op;
+
+		switch (op)
+		{
+		case 1: 
+		{
+			system("cls");
+			cout << "\n Numarul grupei (1,2,3...): ";
+			cin >> nrGp;
+			ACE.printStudentiGrupa(nrGp);
+			break;
+		}
+		case 2:
+		{
+			system("cls");
+
+			short op2;
+			cout << "Adaugi student? <1>\nAdaugi orar? <2>\n";
+			cin >> op2;
+			if (op2 == 1)
+			{
+				cout << "\n Numarul grupei (1,2,3...): ";
+				cin >> nrGp;
+				ACE.addStudent(nrGp);
+			}
+			else
+			{
+				if (op2 == 2)
+				{
+					cout << "\n Numarul grupei (1,2,3...): ";
+					cin >> nrGp;
+					ACE.citesteOrar(nrGp);
+				}
+			}
+			break;
+		}
+		case 3:
+		{
+			system("cls");
+			ACE.printStudentiAfara();
+			break;
+		}
+		case 4:
+		{
+			string nume;
+			system("cls");
+			cin.get();
+		
+			cout << "Numele complet al studentului: ";
+			std::getline(cin, nume);
+			short ret=ACE.cautaStudent(nume);
+			if (ret==-1)
+				cout << "Studentul nu exista!\n";
+			else
+				ACE.printOrarGrupa(ret);
+			break;
+		}
+		case 5:
+		{
+			system("cls");
+			ACE.printMaterieComuna();
+			break;
+		}
+		case 6:
+		{
+			system("cls");
+			CData data;
+			data.read();
+			ACE.printStudentiData(data);
+			break;
+		}
+		default:
+			break;
+		}
+
+
+	}
+
+	system("pause");
+}
---
+++ Tema_3/Student.h
@@ -0,0 +1,28 @@
+#include "Adresa.h"
+#include"Data.h"
+
+class CStudent 
+{
+	string nume;
+	CData dataNasterii;
+	CAdresa adresa;
+
+public:
+	void read(); 
+	void print();
+	inline string getOras() {
+		return adresa.getOras();
+	}
+	inline void printNume() {
+		cout << nume << '\n';
+	}
+	bool testNume(const string& n) const
+	{
+		return n == nume ? 1 : 0;
+	}
+	inline CData getData()
+	{
+		return dataNasterii;
+	}
+};
+
